ffcfab617b13c8971deae332fdae368fe2ebae73
Sonofneli7 <94796497+Sonofneli7@users.noreply.github.com>
2024-11-05 14:37:53 -0500

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { TodosComponent } from './todos/todos.component';
+import { NotesComponent } from './notes/notes.component';
+
+export const routes: Routes = [
+        {path: 'bookmarks', component: BookmarksComponent},
+        {path: 'todos', component: TodosComponent},
+        {path: 'notes', component: NotesComponent},
+        { path: '', redirectTo: '', pathMatch: 'full' },
+];
---
+++ src/app/bookmarks/bookmarks.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookmarks',
+  standalone: true,
+  imports: [],
+  templateUrl: './bookmarks.component.html',
+  styleUrl: './bookmarks.component.scss'
+})
+export class BookmarksComponent {
+
+}
+
+
+// ng s -o   Opens application automatically
