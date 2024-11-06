712e1644eed23a62112f1f70f68df6b3bebc7b81
Sonofneli7 <94796497+Sonofneli7@users.noreply.github.com>
2024-11-05 19:39:21 -0500

    Add files via upload

--- src/app/bookmarks/bookmarks.component.ts
+++ src/app/bookmarks/bookmarks.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BookmarkTileComponent } from "../bookmark-tile/bookmark-tile.component";
 
 @Component({
   selector: 'app-bookmarks',
   standalone: true,
-  imports: [],
+  imports: [BookmarkTileComponent],
   templateUrl: './bookmarks.component.html',
   styleUrl: './bookmarks.component.scss'
 })
