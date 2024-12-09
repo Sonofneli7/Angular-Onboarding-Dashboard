f6eee773ed6cfd1c9f6aef07ca382852b125a520
Sonofneli7 <94796497+Sonofneli7@users.noreply.github.com>
2024-12-09 14:27:31 -0500

    12.9.2024 src updates

--- src/app/shared/todo.model.ts
+++ src/app/shared/todo.model.ts
@@ -1,12 +1,13 @@
-import { v4 as uuidv4 } from "uuid"
+import { v4 as uuidv4 } from 'uuid';
 
+export class Todo {
+  id: string;
+  completed: boolean;
+  noteId: string; // Reference to the note
 
-export class Todo{
-    id: string | undefined
-    completed: boolean | undefined
-
-
-    constructor(public text: string){
-        this.id = uuidv4()
-    }
+  constructor(public text: string) {
+    this.id = uuidv4();
+    this.completed = false;
+    this.noteId = ''; // Initialize noteId with an empty string or some default value
+  }
 }
